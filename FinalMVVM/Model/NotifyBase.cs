5d1054d4ab2ef33dd2eb7061206353e80326cff8
leonardojuchani <42953933+leonardojuchani@users.noreply.github.com>
2020-08-14 20:03:35 -0300

    Programa

---
+++ FinalMVVM/Model/NotifyBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalMVVM.Model
+{
+    /*
+     Se crea la clase NotifyBase para que pase por parámetro el nombre de la propiedad y realice la notificación a la vista
+     cualquier cambio en las propiedades.
+     */
+    public class NotifyBase : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ FinalMVVM/Model/Productos.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalMVVM.Model
+{
+
+    public class Productos : NotifyBase
+    {
+        #region Atributos
+        private int id;
+        private String dueño;
+        private String dni;
+        private String raza;
+        private String descripcion;
+        #endregion
+
+        #region Propiedades
+
+        public int Id
+        {
+            get
+
+            {
+                return id;
+
+            } 
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+
+        } // Fin de propiedad Id
+
+
+        public string Dueño
+        {
+            get
+
+            {
+                return dueño;
+
+            }
+
+            set
+            {
+                dueño = value;
+                OnPropertyChanged("Dueño");
+            }
+        }// Fin de propiedad Dueño
+
+
+        public string Dni
+        {
+            get
+            {
+                return dni;
+            }
+
+            set
+            {
+                dni = value;
+                OnPropertyChanged("Dni");
+            }
+        }// Fin de propiedad Dni
+
+
+        public string Raza
+        {
+            get
+            {
+
+                return raza;
+            }
+
+            set
+            {
+                raza = value;
+                OnPropertyChanged("Raza");
+            }
+        }// Fin de propiedad Raza
+
+
+        public string Descripcion
+        {
+            get
+            {
+                return descripcion;
+
+            }
+            set
+            {
+                descripcion = value;
+                OnPropertyChanged("descipcion");
+            }
+        }// Fin de propiedad Descripcion
+
+        #endregion
+    }
+
+}
+
